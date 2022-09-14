9c998358f858371f5a0e000ad0cae88a431ae945
Kiril Spasov <spasov.kiril@gmail.com>
2022-09-14 08:04:51 +0300

    Add project files.

---
+++ IdentityApp/Authorization/InvoiceAdministratorAuthorizationHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using IdentityApp.Models;
+
+namespace IdentityApp.Authorization
+{
+    public class InvoiceAdministratorAuthorizationHandler :
+        AuthorizationHandler<OperationAuthorizationRequirement, Invoice>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            OperationAuthorizationRequirement requirement,
+            Invoice invoice)
+        {
+            if (context.User == null)
+                return Task.CompletedTask;
+
+            if (context.User.IsInRole(Constants.InvoiceAdministratorRole))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
